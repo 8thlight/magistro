5b9f3b5b14dda7fc390d1ed1c3b509288170ed55
Paul Pagel <paul@8thlight.com>
2011-01-04 20:15:31 -0500

    testing helper file

--- exercises/players/exercises.rb
+++ exercises/players/exercises.rb
@@ -1,9 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + "/../../lib/observer")
 
 on_scene_opened do
-  
-  #TODO - PWP - require all observers in the observer directory
-  ["change_background", "run_output", "run", "navigation", "load_step", "run_all"].each do |observer|
-    require File.expand_path(File.dirname(__FILE__) + "/../observers/#{observer}")
+  Dir.glob(File.expand_path(File.dirname(__FILE__) + "/../observers/*.rb")).each do |file|
+    require file
   end
+  # #TODO - PWP - require all observers in the observer directory
+  # ["change_background", "run_output", "run", "navigation", "load_step", "run_all"].each do |observer|
+  #   require File.expand_path(File.dirname(__FILE__) + "/../observers/#{observer}")
+  # end
 end

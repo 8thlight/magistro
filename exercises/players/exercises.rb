8e3dc4d3d949aff3fc1c3f2aeb0df81165d4316b
Paul Pagel <paul@8thlight.com>
2011-01-04 20:17:57 -0500

    requiring the observers as a glob

--- exercises/players/exercises.rb
+++ exercises/players/exercises.rb
@@ -1,11 +1,3 @@
-require File.expand_path(File.dirname(__FILE__) + "/../../lib/observer")
-
 on_scene_opened do
-  Dir.glob(File.expand_path(File.dirname(__FILE__) + "/../observers/*.rb")).each do |file|
-    require file
-  end
-  # #TODO - PWP - require all observers in the observer directory
-  # ["change_background", "run_output", "run", "navigation", "load_step", "run_all"].each do |observer|
-  #   require File.expand_path(File.dirname(__FILE__) + "/../observers/#{observer}")
-  # end
+  Dir.glob(File.expand_path(File.dirname(__FILE__) + "/../observers/*.rb")).each { |file| require file}
 end
